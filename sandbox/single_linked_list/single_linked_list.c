91c98a830511ff110ebe2eb01ffac9a713d10a8f
Makc <atlanticrev@gmail.com>
2020-08-15 20:00:10 +0300

    complete single linked list

--- sandbox/single_linked_list/single_linked_list.c
+++ sandbox/single_linked_list/single_linked_list.c
@@ -2,65 +2,178 @@
 #include <stdlib.h>
 
 /* List item */
-struct item 
+struct item
 {
   int data;
   struct item *next;
 };
 
+// Add item to the tail of the list
+// struct item *int_array_to_list(int *arr, int len)
+// {
+//   struct item *first = NULL, *last = NULL, *tmp;
+
+//   int i;
+//   for (i = 0; i < len; i++)
+//   {
+//     tmp = malloc(sizeof(struct item));
+//     tmp->data = arr[i];
+//     tmp->next = NULL;
+//     /* If list is not empty */
+//     if (last)
+//     {
+//       last->next = tmp;
+//       last = last->next;
+//     }
+//     else
+//     {
+//       first = last = tmp;
+//     }
+//   }
+// }
+
+// Add item to the head of the list
+// struct item *int_array_to_list(int *arr, int len)
+// {
+//   struct item *first = NULL, *tmp;
+
+//   int i;
+//   for (i = len - 1; i >= 0; i--)
+//   {
+//     tmp = malloc(sizeof(struct item));
+//     tmp->data = arr[i];
+//     tmp->next = first;
+//     first = tmp;
+//   }
+
+//   return first;
+// }
+
+// Recursive case
 struct item *int_array_to_list(int *arr, int len)
 {
-  struct item *first = NULL;
-  struct item *last = NULL;
   struct item *tmp;
-
-  int i;
-  for (i = 0; i < len; i++)
-  {
-    tmp = malloc(sizeof(struct item));
-    tmp->data = arr[i];
-    tmp->next = NULL;
-    /* If list is not empty */
-    if (last)
-    {
-      last->next = tmp;
-      last = last->next;
-    }
-    else
-    {
-      first = last = tmp;
-    }    
-  }
-  return first;  
+  if (!len)
+	return NULL;
+  tmp = malloc(sizeof(struct item));
+  tmp->data = *arr;
+  tmp->next = int_array_to_list(arr + 1, len - 1);
+  return tmp;
 }
 
-void list_walker(struct item *first)
+// Case 0
+// int int_list_sum(const struct item *list)
+// {
+//   int sum = 0;
+//   const struct item *tmp = list;
+//   while(tmp)
+//   {
+//     sum += tmp->data;
+//     tmp = tmp->next;
+//   }
+//   return sum;
+// }
+
+// Case 1
+// int int_list_sum(const struct item *list)
+// {
+//   int sum = 0;
+//   const struct item *tmp = list;
+//   for(tmp = list; tmp; tmp = tmp->next)
+//   {
+//     sum += tmp->data;
+//   }
+//   return sum;
+// }
+
+// Case 2 (Recursive)
+int int_list_sum(const struct item *list)
 {
-  if (!first)
-  {
-    printf("%s\n", "[ERROR] List is empty");
+  return list ? list->data + int_list_sum(list->next) : 0;
+}
+
+// Case 0
+// void delete_int_list(struct item *list)
+// {
+//   while(list)
+//   {
+//     struct item *tmp = list;
+//     list = list->next;
+//     free(tmp);
+//   }
+// }
+
+// Case 1
+// void delete_int_list(struct item *list)
+// {
+//   while(list)
+//   {
+//     struct item *tmp = list->next;
+//     free(list);
+//     list = tmp;
+//   }
+// }
+
+// Case 2 (Recursive)
+void delete_int_list(struct item *list)
+{
+  if (!list)
     return;
-  }
-  /* walk through with a double pionter */
-  struct item **ptr;
-  ptr = &first;
-  int sum = 0;
-  while(*ptr)
+  delete_int_list(list->next);
+  free(list);    
+}
+
+// Case 0
+// void list_filter(struct item **curr)
+// {
+//   /* walk through with a double pointer */
+//   while(*curr)
+//   {
+//     /* do something with list item */
+//     if ((*curr)->data % 2 == 0)
+//     {
+//       struct item *tmp = *curr;
+//       *curr = (*curr)->next;
+//       free(tmp);
+//     }
+//     else
+//     {
+//       printf("%d\n", (*curr)->data);
+//       curr = &(*curr)->next;
+//     }
+//   }
+// }
+
+void list_walker(struct item **curr)
+{
+  while(*curr)
   {
-    /* do something with list item */
-    // printf("%d\n", (*ptr)->data);
-    sum += (*ptr)->data;
-    /* go to the next list item */
-    ptr = &(*ptr)->next;
+    printf("%d\n", (*curr)->data);
+    curr = &(*curr)->next;
   }
-  printf("%d\n", sum);
+}
+
+// Case 1 (Recursive)
+void list_filter(struct item **curr)
+{
+  if (!*curr)
+    return;
+  list_filter(&(*curr)->next);
+  if ((*curr)->data % 2 == 0)
+  {
+    struct item *tmp = *curr;
+    *curr = (*curr)->next;
+    free(tmp);
+  }
 }
 
 int main(int argc, char const **argv)
 {
   enum { arr_len = 10 };
-  int arr[arr_len] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+  int arr[arr_len] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   struct item *list = int_array_to_list(arr, arr_len);
-  list_walker(list);
+  list_filter(&list);
+  list_walker(&list);
+  // printf("%d\n", int_list_sum(list));
   return 0;
 }
